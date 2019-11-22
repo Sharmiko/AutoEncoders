f6f52e2ec41d13f76dc7dbda8a3fbb6ae69ed265
Giorgi Sharmiashvili <sharmiko123@gmail.com>
2019-11-22 20:45:47 +0400

    Some modifications

---
+++ generate.py
@@ -0,0 +1,53 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from Autoencoder import AutoEncoder
+import matplotlib.pyplot as plt
+
+device = ("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Load Testing data dataset
+
+BATCH_SIZE = 32
+
+transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5])])
+
+testset = torchvision.datasets.MNIST(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE,
+                                         shuffle=False, num_workers=2)
+
+
+# Loading the model
+autoencoder = AutoEncoder()
+autoencoder.load_state_dict(torch.load("./model-states/AutoEncoder-[10-Epochs]"))
+autoencoder.to(device)
+
+
+# Extract images and labes from testloader
+images, labels = next(iter(testloader))
+
+# Generate images without grad attribute
+with torch.no_grad():
+    images = images.to(device)
+    encoded_data, decoded = autoencoder(images)
+    
+    
+# Helper function to show images and their corresponding generated ones
+def imshow(inputs, labels, outputs):
+    # create subplots
+    fig, axes = plt.subplots(2, 6, figsize=(13, 5))
+    # plot original images
+    for i in range(6):
+        axes[0, i].imshow(inputs[i].view(28,28).numpy(), cmap="gray")
+        axes[0, i].set_title("Original Number {}".format(labels[i]))
+    
+    # plot generated images
+    for i in range(6):
+        axes[1, i].imshow(outputs[i].view(28,28).numpy(), cmap="gray")
+        axes[1, i].set_title("Encoded Number {}".format(labels[i]))
+
+imshow(images[:6].cpu(), labels[:6], decoded[:6].cpu())
+
--- train.py
+++ train.py
@@ -3,16 +3,13 @@ import torch.nn as nn
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
-
 from Autoencoder import AutoEncoder
-
-import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 
 device = ("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# Load dataset
+# Load training data
 
 BATCH_SIZE = 32
 
@@ -25,10 +22,6 @@ trainset = torchvision.datasets.MNIST(root='./data', train=True,
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=BATCH_SIZE,
                                           shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.MNIST(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE,
-                                         shuffle=False, num_workers=2)
 
 # Create model
 autoencoder = AutoEncoder()
@@ -38,10 +31,9 @@ autoencoder.to(device) # convert model to appropriate device
 optimizer = optim.Adam(autoencoder.parameters())
 criterion = nn.MSELoss()
 
-
 # Training the model
 
-EPOCHS = 5
+EPOCHS = 10
 
 # training loop
 for epoch in tqdm(range(EPOCHS)):
@@ -71,46 +63,4 @@ for epoch in tqdm(range(EPOCHS)):
 print("Total Loss: {}".format(total_loss))
 
 # Save model state
-torch.save(autoencoder.state_dict(), "autoencoder-state")
-
-# Extract images and labes from trainloader
-images, labels = next(iter(trainloader))
-
-# Generate images without grad attribute
-with torch.no_grad():
-    images = images.to(device)
-    encoded_data, decoded = autoencoder(images)
-
-# Helper function to show images and their corresponding generated ones
-def imshow(inputs, labels, outputs):
-    # create subplots
-    fig, axes = plt.subplots(2, 6, figsize=(13, 5))
-    # plot original images
-    for i in range(6):
-        axes[0, i].imshow(inputs[i].view(28,28).numpy(), cmap="gray")
-        axes[0, i].set_title("Original Number {}".format(labels[i]))
-    
-    # plot generated images
-    for i in range(6):
-        axes[1, i].imshow(outputs[i].view(28,28).numpy(), cmap="gray")
-        axes[1, i].set_title("Encoded Number {}".format(labels[i]))
-
-imshow(images[:6].cpu(), labels[:6], decoded[:6].cpu())
-
-
-# Loading the model
-autoencoder = AutoEncoder()
-autoencoder.load_state_dict(torch.load("autoencoder-state"))
-autoencoder.to(device)
-
-
-
-
-
-
-
-
-
-
-
-
+torch.save(autoencoder.state_dict(), "./model-states/AutoEncoder-[{}-Epochs]".format(EPOCHS))
